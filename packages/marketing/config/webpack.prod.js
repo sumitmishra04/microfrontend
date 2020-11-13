25c6aa6cd18d9dbf78478c49fb5af68052e02d36
sumit kumar <sumit-kumar@sumit-kumar.local>
2020-11-13 23:19:58 +0530

    created container deploy script

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederation = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederation({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": `./src/bootstrap`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
